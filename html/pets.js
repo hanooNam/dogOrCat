a3b264d12924bf062dc22c1a73f90cc1e72f247d
hanooNam <jnam920329@gmail.com>
2017-07-21 17:12:16 +0900

    update image from fool to smart

--- html/pets.js
+++ html/pets.js
@@ -6,14 +6,26 @@
       restrict: 'E',
       templateUrl: "pets-image.html",
       controller:function(){
-        this.current = 0;
+        this.current ;
 
+        // // this.goImage = function(image){
+        // //   if (this.current !== null){
+        // //     return this.current = pet.images[0]
+        // //   }else{
+        // //     return this.current = image;
+        // //   }
+        // // };
+        // this.basic = function(pet){
+        //   console.log(pet);
+        //   return this.current = pet.images[0];
+        // };
+        // 이렇게나 쓸모없는 코드가 많았음 ㅎ
         this.clicked = function(image){
-          var num = image.slice(-5,-4);
-          num = num > 3 ? image.slice(-5,-4) - 4 : image.slice(-5,-4) -1;
-          this.current = num;
+          // var num = image.slice(-5,-4);
+          // num = num > 3 ? image.slice(-5,-4) - 4 : image.slice(-5,-4) -1;
+          // this.current = num;
+          return this.current = image;
         };
-
       },
       controllerAs:'img'
     };
